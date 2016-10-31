6330289695a859091e58e0471df10bdffec408f4
Rafał Witowski <witowski.r@gmail.com>
2016-10-31 20:57:04 +0100

    typelist::apply

--- src/rafalw/typelist.hpp
+++ src/rafalw/typelist.hpp
@@ -2,22 +2,13 @@
 #define RAFALW_TYPELIST_HPP_
 
 #include <rafalw/templates/compose.hpp>
+#include <rafalw/utils/Type.hpp>
 #include <type_traits>
 #include <typeindex>
 #include <typeinfo>
 
 inline namespace rafalw {
 namespace typelist {
-
-template<typename... Types>
-struct List {};
-
-using empty = List<>;
-
-struct NotFound {};
-
-template<std::size_t V>
-struct Index : public std::integral_constant<std::size_t, V> {};
 
 namespace detail {
 
@@ -56,34 +47,155 @@ namespace detail {
     // isinstance
 
     template<typename T>
+    struct IsInstance;
+
+    template<typename L>
+    struct Length;
+
+    template<typename L>
+    struct Tail;
+
+    template<typename L>
+    struct Head;
+
+    template<typename L, typename Element>
+    struct Append;
+
+    template<typename L, typename Element>
+    struct Prepend;
+
+    template<typename L, template<typename> class... F>
+    struct Map;
+
+    template<typename L, template<typename, typename> class F>
+    struct Reduce;
+
+    template<typename L1, typename L2>
+    struct Concat2;
+
+    template<typename L>
+    struct Reverse;
+
+    template<typename L, typename Element, std::size_t I>
+    struct Find;
+
+    template<typename L, template<typename...> class F>
+    struct Filter;
+
+    template<typename L>
+    struct Unique;
+
+    template<typename L, std::size_t N>
+    struct Drop;
+
+    template<bool empty, typename... Ls>
+    struct Zip;
+
+    template<typename L, typename F>
+    auto do_apply(F&& f, std::true_type) -> void;
+
+    template<typename L, typename F>
+    auto do_apply(F&& f, std::false_type) -> void;
+
+    template<typename L1, typename L2>
+    using concat2 = result<Concat2<L1, L2>>;
+
+} // namespace detail
+
+template<typename... Types>
+struct List {};
+
+struct NotFound {};
+
+template<std::size_t V>
+struct Index : public std::integral_constant<std::size_t, V> {};
+
+// typelist functions
+
+template<typename T>
+constexpr auto isinstance = detail::IsInstance<T>::value;
+
+template<typename List>
+constexpr auto length = detail::Length<List>::value;
+
+template<typename L>
+constexpr auto isempty = length<L> == 0;
+
+using empty = List<>;
+
+template<typename L>
+using tail = detail::result<detail::Tail<L>>;
+
+template<typename L>
+using head = detail::result<detail::Head<L>>;
+
+template<typename L, typename E>
+using append = detail::result<detail::Append<L, E>>;
+
+template<typename L, typename E>
+using prepend = detail::result<detail::Prepend<L, E>>;
+
+template<typename L, template<typename> class... F>
+using map = detail::result<detail::Map<L, F...>>;
+
+template<typename L, template<typename, typename> class F>
+using reduce = detail::result<detail::Reduce<L, F>>;
+
+template<typename... Ls>
+using concat = reduce<List<empty, Ls...>, detail::concat2>;
+
+template<typename L>
+using reverse = detail::result<detail::Reverse<L>>;
+
+template<typename L, typename E, std::size_t I = 0>
+using find = detail::result<detail::Find<L, E, I>>;
+
+template<typename L, template<typename...> class F>
+using filter = detail::result<detail::Filter<L, F>>;
+
+template<typename L, typename E>
+using remove = filter<L, detail::Bind<detail::Not<std::is_same>::template Result, E>::template Result>;
+
+template<typename L>
+using unique = detail::result<detail::Unique<L>>;
+
+template<typename L, std::size_t N>
+using drop = detail::result<detail::Drop<L, N>>;
+
+template<typename L, std::size_t N>
+using take = reverse<drop<reverse<L>, length<L> - N>>;
+
+template<typename L, std::size_t I>
+using get = head<drop<L, I>>;
+
+template<typename L, std::size_t I>
+using erase = concat<take<L, I>, drop<L, I + 1>>;
+
+template<typename... Ls>
+using zip = detail::result<detail::Zip<detail::any<isempty<Ls>...>, Ls...>>;
+
+template<typename L, typename E>
+constexpr auto contains = !std::is_same<find<L, E>, NotFound>::value;
+
+template<typename L, typename E>
+constexpr auto count = length<filter<L, detail::Bind<std::is_same, E>::template Result>>;
+
+template<typename L, typename F>
+auto apply(F&& f) -> void
+{
+    detail::do_apply<L>(std::forward<F>(f), std::integral_constant<bool, !isempty<L>>{});
+}
+
+namespace detail {
+
+    template<typename T>
     struct IsInstance : public std::false_type {};
 
     template<typename... E>
     struct IsInstance<List<E...>> : public std::true_type {};
 
-    template<typename T>
-    constexpr auto isinstance = IsInstance<T>::value;
-
-    // length
-
-    template<typename L>
-    struct Length;
-
     template<typename... Elements>
     struct Length<List<Elements...>> : public std::integral_constant<std::size_t, sizeof...(Elements)> {};
-
-    template<typename List>
-    constexpr auto length = Length<List>::value;
-
-    // isempty
-
-    template<typename L>
-    constexpr auto isempty = length<L> == 0;
-
-    // tail
-
-    template<typename L>
-    struct Tail;
 
     template<typename Element0, typename... Elements>
     struct Tail<List<Element0, Elements...>>
@@ -91,29 +203,11 @@ namespace detail {
         using Result = List<Elements...>;
     };
 
-    template<typename List>
-    using tail = result<Tail<List>>;
-
-
-    // head
-
-    template<typename L>
-    struct Head;
-
     template<typename Element0, typename... Elements>
     struct Head<List<Element0, Elements...>>
     {
         using Result = Element0;
     };
-
-    template<typename List>
-    using head = result<Head<List>>;
-
-
-    // append
-
-    template<typename L, typename Element>
-    struct Append;
 
     template<typename... Elements, typename Element>
     struct Append<List<Elements...>, Element>
@@ -121,31 +215,11 @@ namespace detail {
         using Result = List<Elements..., Element>;
     };
 
-    template<typename List, typename Element>
-    using append = result<Append<List, Element>>;
-
-
-    // prepend
-
-    template<typename L, typename Element>
-    struct Prepend;
-
     template<typename... Elements, typename Element>
     struct Prepend<List<Elements...>, Element>
     {
         using Result = List<Element, Elements...>;
     };
-
-    template<typename List, typename Element>
-    using prepend = result<Prepend<List, Element>>;
-
-    // map
-
-    template<typename L, template<typename> class... F>
-    struct Map;
-
-    template<typename L, template<typename> class... F>
-    using map = result<Map<L, F...>>;
 
     template<typename L, template<typename> class... F>
     struct Map
@@ -159,14 +233,6 @@ namespace detail {
         using Result = empty;
     };
 
-    // reduce
-
-    template<typename L, template<typename, typename> class F>
-    struct Reduce;
-
-    template<typename L, template<typename, typename> class F>
-    using reduce = result<Reduce<L, F>>;
-
     template<typename E1, typename E2, typename... E, template<typename, typename> class F>
     struct Reduce<List<E1, E2, E...>, F>
     {
@@ -179,30 +245,11 @@ namespace detail {
         using Result = E;
     };
 
-    // concat
-
-    template<typename L1, typename L2>
-    struct Concat2;
-
     template<typename... Elements1, typename... Elements2>
     struct Concat2<List<Elements1...>, List<Elements2...>>
     {
         using Result = List<Elements1..., Elements2...>;
     };
-
-    template<typename L1, typename L2>
-    using concat2 = result<Concat2<L1, L2>>;
-
-    template<typename... L>
-    using concat = reduce<List<empty, L...>, concat2>;
-
-    // reverse
-
-    template<typename L>
-    struct Reverse;
-
-    template<typename List>
-    using reverse = result<Reverse<List>>;
 
     template<typename L>
     struct Reverse
@@ -215,14 +262,6 @@ namespace detail {
     {
         using Result = empty;
     };
-
-    // find
-
-    template<typename L, typename Element, std::size_t I>
-    struct Find;
-
-    template<typename List, typename Element, std::size_t I = 0>
-    using find = result<Find<List, Element, I>>;
 
     template<typename L, typename Element, std::size_t I>
     struct Find
@@ -242,19 +281,6 @@ namespace detail {
         using Result = NotFound;
     };
 
-    // contains
-
-    template<typename List, typename Element>
-    constexpr auto contains = !std::is_same<find<List, Element>, NotFound>::value;
-
-    // filter
-
-    template<typename L, template<typename...> class F>
-    struct Filter;
-
-    template<typename L, template<typename...> class F>
-    using filter = result<Filter<L, F>>;
-
     template<typename L, template<typename...> class F>
     struct Filter
     {
@@ -268,24 +294,6 @@ namespace detail {
         using Result = empty;
     };
 
-    // remove
-
-    template<typename L, typename E>
-    using remove = filter<L, Bind<Not<std::is_same>::template Result, E>::template Result>;
-
-    // count
-
-    template<typename L, typename E>
-    static constexpr auto count = length<filter<L, Bind<std::is_same, E>::template Result>>;
-
-    // unique
-
-    template<typename L>
-    struct Unique;
-
-    template<typename L>
-    using unique = result<Unique<L>>;
-
     template<typename L>
     struct Unique
     {
@@ -297,14 +305,6 @@ namespace detail {
     {
         using Result = empty;
     };
-
-    // drop
-
-    template<typename L, std::size_t N>
-    struct Drop;
-
-    template<typename List, std::size_t COUNT>
-    using drop = result<Drop<List, COUNT>>;
 
     template<typename L, std::size_t N>
     struct Drop
@@ -318,29 +318,6 @@ namespace detail {
         using Result = L;
     };
 
-    // take
-
-    template<typename L, std::size_t COUNT>
-    using take = reverse<drop<reverse<L>, length<L> - COUNT>>;
-
-    // element
-
-    template<typename L, std::size_t N>
-    using get = head<drop<L, N>>;
-
-    // erase
-
-    template<typename L, std::size_t INDEX>
-    using erase = concat<take<L, INDEX>, drop<L, INDEX + 1>>;
-
-    // zip
-
-    template<bool empty, typename... Ls>
-    struct Zip;
-
-    template<typename... Ls>
-    using zip = result<Zip<any<isempty<Ls>...>, Ls...>>;
-
     template<typename... Ls>
     struct Zip<true, Ls...>
     {
@@ -353,73 +330,17 @@ namespace detail {
         using Result = concat<List<List<head<Ls>...>>, zip<tail<Ls>...>>;
     };
 
+    template<typename L, typename F>
+    auto do_apply(F&& f, std::true_type) -> void
+    {
+        f(utils::type<head<L>>);
+        apply<tail<L>>(std::forward<F>(f));
+    }
+
+    template<typename L, typename F>
+    auto do_apply(F&&, std::false_type) -> void {}
+
 } // namespace detail
-
-template<typename T>
-static constexpr auto isinstance = detail::isinstance<T>;
-
-template<typename L>
-static constexpr auto length = detail::length<L>;
-
-template<typename L>
-static constexpr auto isempty = detail::isempty<L>;
-
-template<typename L, typename E>
-static constexpr auto contains = detail::contains<L, E>;
-
-template<typename L, typename E>
-static constexpr auto count = detail::count<L, E>;
-
-template<typename L, std::size_t I>
-using get = detail::get<L, I>;
-
-template<typename L>
-using head = detail::head<L>;
-
-template<typename L>
-using tail = detail::tail<L>;
-
-template<typename L, typename E>
-using append = detail::append<L, E>;
-
-template<typename L, typename E>
-using prepend = detail::prepend<L, E>;
-
-template<typename L>
-using reverse = detail::reverse<L>;
-
-template<typename... L>
-using concat = detail::concat<L...>;
-
-template<typename L, typename E>
-using find = detail::find<L, E>;
-
-template<typename L>
-using unique = detail::unique<L>;
-
-template<typename L, std::size_t N>
-using drop = detail::drop<L, N>;
-
-template<typename L, std::size_t N>
-using take = detail::take<L, N>;
-
-template<typename L, std::size_t Idx>
-using erase = detail::erase<L, Idx>;
-
-template<typename L, typename E>
-using remove = detail::remove<L, E>;
-
-template<typename L, template<typename> class F>
-using filter = detail::filter<L, F>;
-
-template<typename L, template<typename> class... F>
-using map = detail::map<L, F...>;
-
-template<typename L, template<typename, typename> class F>
-using reduce = detail::reduce<L, F>;
-
-template<typename... Ls>
-using zip = detail::zip<Ls...>;
 
 } // namespace typelist
 } // namespace rafalw
