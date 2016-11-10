81d29d2335156ca43edce7659752cd3bbb80653e
Rafal Witowski <rafalw@trdlnk.com>
2016-11-10 12:06:06 +0100

    rafalw_utils_assert - fix and simplify

--- src/rafalw/utils/assert.hpp
+++ src/rafalw/utils/assert.hpp
@@ -4,80 +4,26 @@
 #include <rafalw/utils/Error.hpp>
 #include <cstdlib>
 
-#if !defined(RAFALW_UTILS_ASSERT_MODE) || (RAFALW_UTILS_ASSERT_MODE < 0) || (RAFALW_UTILS_ASSERT_MODE > 2)
-#undef RAFALW_UTILS_ASSERT_MODE
-#define RAFALW_UTILS_ASSERT_MODE 0
-#endif
-
 inline namespace rafalw {
 namespace utils {
 
 class AssertionError : public Error
 {
 public:
-    AssertionError(const char* file, int line, const char* condition) :
-        Error{ "assertion failed - ", condition, " in ", file, ":", line }
+    AssertionError(const char* file, const char* func, int line, const char* condition) :
+        Error{ "assertion `", condition, "' failed in ", file, ":", line, " [", func, "]" }
     {}
 };
-
-namespace detail {
-
-    enum class AssertionMode : char
-    {
-        DISABLED = 0,
-        ABORT = 1,
-        THROW = 2
-    };
-
-    template<AssertionMode T>
-    struct AssertionHandler;
-
-    template<>
-    struct AssertionHandler<AssertionMode::DISABLED>
-    {
-        static auto failed(const char* /*file*/, int /*line*/, const char* /*cond*/) -> void
-        {}
-    };
-
-    template<>
-    struct AssertionHandler<AssertionMode::ABORT>
-    {
-        static auto failed(const char* /*file*/, int /*line*/, const char* /*cond*/) -> void
-        {
-            std::abort();
-        }
-    };
-
-    template<>
-    struct AssertionHandler<AssertionMode::THROW>
-    {
-        static auto failed(const char* file, int line, const char* cond) -> void
-        {
-            throw AssertionError{ file, line, cond };
-        }
-    };
-
-    #if (RAFALW_UTILS_ASSERT_MODE == 0)
-        #define rafalw_utils_detail_assertion_check_failed(cond) false
-    #else
-        #define rafalw_utils_detail_assertion_check_failed(cond) !(cond)
-    #endif
-
-    constexpr auto ASSERTION_MODE = static_cast<AssertionMode>(RAFALW_UTILS_ASSERT_MODE);
-
-    auto assertion_failed(const char* file, int line, const char* cond) -> void
-    {
-        AssertionHandler<ASSERTION_MODE>::failed(file, line, cond);
-    }
-}
 
 } // namespace utils
 } // namespace rafalw
 
-#define rafalw_utils_assert(cond) \
-    do { \
-        if (rafalw_utils_detail_assertion_check_failed(cond)) \
-            ::rafalw::utils::detail::assertion_failed(__FILE__, __LINE__, #cond); \
-    } while (false)
+#if !defined(RAFALW_UTILS_ASSERT_MODE) || (RAFALW_UTILS_ASSERT_MODE == 0)
+#define rafalw_utils_assert(cond)
+#elif RAFALW_UTILS_ASSERT_MODE == 1
+#define rafalw_utils_assert(cond) assert(cond)
+#elif RAFALW_UTILS_ASSERT_MODE == 2
+#define rafalw_utils_assert(cond) throw rafalw::utils::AssertionError{ __FILE__, __PRETTY_FUNCTION__, __LINE__, #cond }
+#endif
 
 #endif // RAFALW_UTILS_ASSERT_HPP_
