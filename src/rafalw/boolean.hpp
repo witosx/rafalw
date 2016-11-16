885553d4f2080718e2338855cb5fb1c22a56878a
Rafal Witowski <rafalw@trdlnk.com>
2016-11-16 16:04:11 +0100

    boolean::any

---
+++ src/rafalw/boolean.hpp
@@ -0,0 +1,24 @@
+#ifndef RAFALW_BOOLEAN_HPP_
+#define RAFALW_BOOLEAN_HPP_
+
+inline namespace rafalw {
+namespace boolean {
+
+auto any() -> bool
+{
+    return false;
+}
+
+template<typename T, typename... Args>
+auto any(const T& arg0, const Args&... args) -> bool
+{
+    if (static_cast<bool>(arg0))
+        return true;
+
+    return any(args...);
+}
+
+}
+}
+
+#endif // RAFALW_BOOLEAN_HPP_
