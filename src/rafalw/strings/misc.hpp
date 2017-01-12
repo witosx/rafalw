e284e0b6254437e76b3bdad914d55b8704aef744
Rafal Witowski <rafalw@trdlnk.com>
2017-01-12 11:48:57 +0100

    move strings::Joiner to separate file

--- src/rafalw/strings/misc.hpp
+++ src/rafalw/strings/misc.hpp
@@ -50,53 +50,6 @@ auto remove(std::basic_string<Char> str, Char c) -> std::basic_string<Char>
 }
 
 template<typename Char>
-class Joiner
-{
-public:
-    Joiner(const Char* s) :
-        m_separator{ s }
-    {}
-
-    template<typename... Strs>
-    auto operator ()(std::basic_string<Char> str0, const Strs&... strs) const -> std::basic_string<Char>
-    {
-        utils::static_foreach(std::forward_as_tuple(strs...), [this,&str0](const auto& s){
-            str0 += m_separator;
-            str0 += s;
-        });
-
-        return str0;
-    }
-
-    template<typename... Strs>
-    auto operator ()(const Char* str0, const Strs&... strs) const -> std::basic_string<Char>
-    {
-        return operator ()(std::basic_string<Char>{ str0 }, strs...);
-    }
-
-    template<typename... Strs>
-    auto operator ()(Char c, const Strs&... strs) const -> std::basic_string<Char>
-    {
-        return operator ()(std::basic_string<Char>(1, c), strs...);
-    }
-
-private:
-    const Char* m_separator;
-};
-
-template<typename Char>
-auto joiner(const Char* s) -> Joiner<Char>
-{
-    return Joiner<Char>{ s };
-}
-
-template<typename... Strs>
-auto join(Strs&&... strs) -> std::string
-{
-    return joiner("")(std::move(strs)...);
-}
-
-template<typename Char>
 auto split(const std::basic_string<Char>& s, Char c) -> std::vector<std::basic_string<Char>>
 {
     auto res = std::vector<std::basic_string<Char>>{};
