6ad63e9c036c3d5b69d8eaeda25f9871b96844de
Rafal Witowski <rafalw@trdlnk.com>
2017-04-25 15:09:09 +0200

    generator::Join simplified, add operator +

--- src/rafalw/generator/compound/Join.hpp
+++ src/rafalw/generator/compound/Join.hpp
@@ -2,79 +2,118 @@
 #define RAFALW_GENERATOR_JOIN_HPP_
 
 #include <rafalw/generator/Base.hpp>
-#include <rafalw/utils/static.hpp>
-#include <rafalw/tuple.hpp>
-#include <tuple>
 
 inline namespace rafalw {
 namespace generator {
 
-template<typename... GeneratorsT>
+namespace detail {
+
+    template<typename T>
+    struct JoinValueTypeBase
+    {
+        using Type = T;
+    };
+
+    template<typename T1, typename T2>
+    struct JoinValueType;
+
+    template<typename T> struct JoinValueType<T, T>         : public JoinValueTypeBase<T> {};
+
+    template<typename T> struct JoinValueType<T, T&>        : public JoinValueTypeBase<T> {};
+    template<typename T> struct JoinValueType<T, const T&>  : public JoinValueTypeBase<T> {};
+    template<typename T> struct JoinValueType<T&, T>        : public JoinValueTypeBase<T> {};
+    template<typename T> struct JoinValueType<const T&, T>  : public JoinValueTypeBase<T> {};
+
+    template<typename T> struct JoinValueType<const T&, T&> : public JoinValueTypeBase<const T&> {};
+    template<typename T> struct JoinValueType<T&, const T&> : public JoinValueTypeBase<const T&> {};
+
+    template<typename G1, typename G2>
+    using JoinValue = typename JoinValueType<decltype(peek(std::declval<G1>())), decltype(peek(std::declval<G2>()))>::Type;
+
+} // namespace detail
+
+template<typename GeneratorT1, typename GeneratorT2, typename ValueT = detail::JoinValue<GeneratorT1, GeneratorT2>>
 class Join : private Base
 {
 public:
-    template<typename... GeneratorsT2>
-    Join(ConstructTag, GeneratorsT2&&... generators) :
-        m_generators{ std::forward<GeneratorsT2>(generators)... }
+    template<typename G1, typename G2>
+    Join(G1&& g1, G2&& g2) :
+        m_generator1{ std::forward<G1>(g1) },
+        m_generator2{ std::forward<G2>(g2) }
     {}
 
 private:
     friend class BaseAccess;
 
-    std::tuple<GeneratorsT...> m_generators;
+    using Generator1 = GeneratorT1;
+    using Generator2 = GeneratorT2;
 
-    using References = std::array<ErasedReference<decltype(peek(std::get<0>(m_generators)))>, sizeof...(GeneratorsT)>;
-    References m_references{{ std::get<GeneratorsT>(m_generators)... }};
+    using Value = ValueT;
 
-    typename References::iterator m_current = m_references.begin();
+    Generator1 m_generator1;
+    Generator2 m_generator2;
+
+    int m_index = 0;
 
     auto generatorDone() const -> bool
     {
-        return m_current == end(m_references);
+        return m_index == 2;
     }
 
-    decltype(auto) generatorPeek() const
+    auto generatorPeek() const -> Value
     {
-        return peek(*m_current);
+        if (m_index == 0)
+            return peek(m_generator1);
+        else
+            return peek(m_generator2);
     }
 
     auto generatorUpdate() -> void
     {
-        update(*m_current);
-
-        if (done(*m_current) && m_current != end(m_references))
-            m_current++;
+        if (m_index == 0)
+            updateGenerator(m_generator1);
+        else
+            updateGenerator(m_generator2);
     }
 
     auto generatorReset()
     {
-        constexpr auto resetable = std::conjunction<std::bool_constant<has_reset<GeneratorsT>>...>::value;
+        static constexpr auto RES1 = decltype(try_reset(m_generator1))::value;
+        static constexpr auto RES2 = decltype(try_reset(m_generator2))::value;
+        static constexpr auto RES = RES1 && RES2;
 
-        if constexpr (resetable)
+        if (RES)
         {
-            utils::static_foreach(m_generators, [](auto& gen){
-                reset(gen);
-            });
+            try_reset(m_generator1);
+            try_reset(m_generator2);
+            m_index = 0;
+        }
 
-            m_current = m_references.begin();
-            return RESET_OK;
-        }
-        else
-        {
-            return RESET_UNAVAILABLE;
-        }
+        return ResetTag<RES>{};
+    }
+
+    template<typename G>
+    auto updateGenerator(G& g) -> void
+    {
+        update(g);
+        if (done(g))
+            m_index++;
     }
 };
 
-template<typename... GeneratorsT>
-auto join(GeneratorsT&&... generators) -> Join<std::decay_t<GeneratorsT>...>
+template<typename GeneratorT1, typename GeneratorT2>
+auto join(GeneratorT1&& g1, GeneratorT2&& g2) -> Join<std::decay_t<GeneratorT1>, std::decay_t<GeneratorT2>>
 {
-    return Join<std::decay_t<GeneratorsT>...>{ ConstructTag{}, std::forward<GeneratorsT>(generators)... };
+    return Join<std::decay_t<GeneratorT1>, std::decay_t<GeneratorT2>>{ std::forward<GeneratorT1>(g1), std::forward<GeneratorT2>(g2) };
+}
+
+template<typename GeneratorT1, typename GeneratorT2, require_instance<GeneratorT1> = nullptr, require_instance<GeneratorT2> = nullptr>
+auto operator +(GeneratorT1&& g1, GeneratorT2&& g2) -> decltype(join(std::forward<GeneratorT1>(g1), std::forward<GeneratorT2>(g2)))
+{
+    return join(std::forward<GeneratorT1>(g1), std::forward<GeneratorT2>(g2));
 }
 
 } // namespace generator
 } // namespace rafalw
-
-
 
 #endif // RAFALW_GENERATOR_JOIN_HPP_
